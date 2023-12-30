a8c7ab96e7903aaca6780da45beba341d9772d06
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-29 18:52:51 -0800

    removes redundant SimpleStorage contract and creates an inhertied contract

--- solidity/SimpleStorage/SimpleStorage.sol
+++ solidity/SimpleStorage/SimpleStorage.sol
@@ -1,45 +1,27 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.19;
 
 contract SimpleStorage {
-    // storage variable
-    // by default since outside of a function
-    uint256 myfavoriteNumber;
+    uint256 myFavoriteNumber;
 
-    // uint256[] listOfFavoriteNumbers;
-
-    // user-defined type
     struct Person {
         uint256 favoriteNumber;
         string name;
     }
-
-    // array
-    // public keyword exposes the variable
+    // uint256[] public anArray;
     Person[] public listOfPeople;
 
-    // dictionary
     mapping(string => uint256) public nameToFavoriteNumber;
 
-    // Person public pat = Person({favoriteNumber: 7, name: "Pat"});
-    
-
-    function store(uint256 _favoriteNumber) public {
-        myfavoriteNumber = _favoriteNumber;
+    function store(uint256 _favoriteNumber) public virtual {
+        myFavoriteNumber = _favoriteNumber;
     }
 
-    // view: state can only be read and not modified
-    // pure: state can neither be read nor modified
-    // calling pure and view functions directly doesnt cost any gas
-    // gas is spent only if pure or view functions are called by another gas costing function
-    function retrieve() public view returns(uint256) {
-        return myfavoriteNumber;
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
     }
 
-    // calldata - immutable temporary variables
-    // memory - mutable temporary variables
-    // storage - mutable permanent variable
-    // such keywords are only required for reference data types: string, array, struct
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         listOfPeople.push(Person(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;

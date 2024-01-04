8e4fb17ddacf87723d141901d3f2e5207699f06d
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-03 18:46:13 -0800

    implements the withdraw function in FundMe contract

--- solidity/FundMe/FundMe.sol
+++ solidity/FundMe/FundMe.sol
@@ -10,15 +10,56 @@ import { PriceConverter } from "./PriceConverter.sol";
 
 contract FundMe {
     using PriceConverter for uint256;
-    
+
     uint256 public minimumUSD = 50 * 1e18;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumUSD, "Didn't send enough!");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        // reset amounts
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        
+        // reset the funders list
+        funders = new address[](0);
+
+        // actually withdraw the funds
+        // msg.sender = type address
+        // payable(msg.sender) = type payable address
+        
+        // transfer
+        // success or throws error
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // send
+        // returns a boolean if success or failure
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        
+        // call
+        // can call any function
+        // recommended
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed!");
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
     }
 }

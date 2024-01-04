e8bfb5f08ead99dce85acf3a89806af9bd94385f
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-03 18:18:01 -0800

    implements library PriceConverter for FundMe

---
+++ solidity/FundMe/PriceConverter.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+// install @chainlink/contracts dependency for import to work
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice() internal view returns(uint256) {
+        // ABI
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        (,int256 answer,,,) = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).latestRoundData();
+        return uint256(answer * 1e10);
+    }
+
+    function getVersion() internal view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUSD;
+    }
+}
